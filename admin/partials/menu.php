6f118642fb1861fbf97523b23fe87cdcb735e3db
Chanuka999 <chanukaranditha99@gmail.com>
2024-10-18 21:24:47 -0700

    new

--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -10,11 +10,11 @@
     <div class="menu text-center">
        <div class="wrapper">
       <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Admin</a></li>
-        <li><a href="#">Category</a></li>
-        <li><a href="#">Food</a></li>
-        <li><a href="#">Order</a></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="manege-admin.php">Admin</a></li>
+        <li><a href="manege-category.php">Category</a></li>
+        <li><a href="manege-food.php">Food</a></li>
+        <li><a href="manege-order.php">Order</a></li>
       </ul>
        </div>
     </div>
