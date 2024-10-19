cb36a64fe68b05da747671fa1e60fec03bb73a9f
Chanuka999 <chanukaranditha99@gmail.com>
2024-10-18 19:31:25 -0700

    update file

--- admin/index.php
+++ admin/index.php
@@ -1,34 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>food-order</title>
-    <link rel="stylesheet" href="../css/admin.css">
-</head>
-<body>
-    <div class="menu text-center">
-       <div class="wrapper">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Admin</a></li>
-        <li><a href="#">Category</a></li>
-        <li><a href="#">Food</a></li>
-        <li><a href="#">Order</a></li>
-      </ul>
-       </div>
-    </div>
+<?php  include('partials/menu.php'); ?>
 
     <div class="main-content">
     <div class="wrapper">
-       main content goes here
+      <h1>DASHBOARD</h1>
+       <div class="col-4 text-center">
+       <h1>5</h1>
+       categories
        </div>
-    </div>
 
-    <div class="footer">
-    <div class="wrapper">
-      <p class="text-center">2024 ALL RIGHT RESERVED,Some Restuarant Developed By.<a href="#">CHANUKA RANDITHA</a></p>
+       <div class="col-4 text-center">
+       <h1>5</h1>
+       categories
        </div>
+
+       <div class="col-4 text-center">
+       <h1>5</h1>
+       categories
+       </div>
+
+       <div class="col-4 text-center">
+       <h1>5</h1>
+       categories
+       </div>
+       <div class="clearfix"></div>
+       </div>
+      
     </div>
-</body>
-</html>
+    
+
+   <?php include('partials/footer.php')  ?>
