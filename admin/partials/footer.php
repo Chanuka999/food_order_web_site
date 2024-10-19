fc2325dda3c8a91b2884dbb5ac1b3c912c025104
Chanuka999 <chanukaranditha99@gmail.com>
2024-10-18 19:31:37 -0700

    footer section

---
+++ admin/partials/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+    <div class="wrapper">
+      <p class="text-center">2024 ALL RIGHT RESERVED,Some Restuarant Developed By.<a href="#">CHANUKA RANDITHA</a></p>
+       </div>
+    </div>
+</body>
+</html>
