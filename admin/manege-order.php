55e0b0f6a64cabfc01af56e938738968eeda5af8
Chanuka999 <chanukaranditha99@gmail.com>
2024-10-19 13:38:30 -0700

    order file update

--- admin/manege-order.php
+++ admin/manege-order.php
@@ -3,6 +3,57 @@
 <div class="main-content">
    <div class="wrapper">
    <h1>MANEGE ORDER</h1>
+  
+<br><br><br>
+<table class="tbl-full">
+  <tr>
+    <th>S.N</th>
+    <th>Full Name</th>
+    <th>Username</th>
+    <th>Action</th>
+  </tr>
+
+  <tr>
+    <td>1</td>
+    <td>chanuka</td>
+    <td>chanuu</td>
+    <td>
+      <a href="#" class="btn-secondery">Update account</a>
+      <a href="#" class="btn-danger">delete account</a>
+    </td>
+  </tr>
+
+  <tr>
+    <td>1</td>
+    <td>chanuka</td>
+    <td>chanuu</td>
+    <td>
+    <a href="#" class="btn-secondery">Update account</a>
+    <a href="#" class="btn-danger">delete account</a>
+    </td>
+  </tr>
+
+  <tr>
+    <td>1</td>
+    <td>chanuka</td>
+    <td>chanuu</td>
+    <td>
+    <a href="#" class="btn-secondery">Update account</a>
+    <a href="#" class="btn-danger">delete account</a>
+    </td>
+  </tr>
+
+  <tr>
+    <td>1</td>
+    <td>chanuka</td>
+    <td>chanuu</td>
+    <td>
+    <a href="#" class="btn-secondery">Update account</a>
+    <a href="#" class="btn-danger">delete account</a>
+    </td>
+  </tr>
+</table>
+
    </div>
 </div>
 
