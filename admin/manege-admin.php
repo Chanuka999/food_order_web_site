b0a1924744af182f4e182ddab91bd5900c0a6b4d
Chanuka999 <chanukaranditha99@gmail.com>
2024-10-19 10:13:49 -0700

    update

--- admin/manege-admin.php
+++ admin/manege-admin.php
@@ -3,7 +3,57 @@
     <div class="main-content">
     <div class="wrapper">
       <h1>MANEGE ADMIN</h1>
-      
+      <br><br>
+
+      <a href="#" class="btn-primary">Add admin</a>
+      <table class="tbl-full">
+        <tr>
+          <th>S.N</th>
+          <th>Full Name</th>
+          <th>Username</th>
+          <th>Action</th>
+        </tr>
+
+        <tr>
+          <td>1</td>
+          <td>chanuka</td>
+          <td>chanuu</td>
+          <td>
+            update account
+            delete account
+          </td>
+        </tr>
+
+        <tr>
+          <td>1</td>
+          <td>chanuka</td>
+          <td>chanuu</td>
+          <td>
+            update account
+            delete account
+          </td>
+        </tr>
+
+        <tr>
+          <td>1</td>
+          <td>chanuka</td>
+          <td>chanuu</td>
+          <td>
+            update account
+            delete account
+          </td>
+        </tr>
+
+        <tr>
+          <td>1</td>
+          <td>chanuka</td>
+          <td>chanuu</td>
+          <td>
+            update account
+            delete account
+          </td>
+        </tr>
+      </table>
       
     </div>
    </div>
