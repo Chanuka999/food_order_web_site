e81d52cfab616fcb2606077d0a1581f86219803b
Chanuka999 <chanukaranditha99@gmail.com>
2024-10-19 13:39:03 -0700

    connected database

---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServerName= "localhost";
+$dbUserName ="root";
+$dbPassword = "";
+$dbName="food-order";
+
+$conn = mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName) or die();
+
+?>
