7f4d0340d55bed94b73c6500fdb92a7de8b3e339
Chanuka999 <chanukaranditha99@gmail.com>
2024-10-22 23:00:20 -0700

    apply form method

--- admin/login.php
+++ admin/login.php
@@ -3,12 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>login-food order system</title>
     <link rel="stylesheet" href="../css/admin.css">
 </head>
 <body>
     <div class="login">
-        <h1>Login</h1>
+        <h1 class="text-center">Login</h1>
+
+        <form action="" method="POST">
+            <label>USERNAME</label>
+            <input type="text" name="username" placeholder="username">
+        </form>
+        <p class="text-center">Created by-<a href="">CHANUKA RANDITHA</a></p>
     </div>
 </body>
 </html>
