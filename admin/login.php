364ebc17d9301ef34d9426695093491d4f27359b
Chanuka999 <chanukaranditha99@gmail.com>
2024-10-24 11:01:18 -0700

    apply form to login page

--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,5 @@
+<?php include('../config/constants.php');  ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,12 +11,51 @@
 <body>
     <div class="login">
         <h1 class="text-center">Login</h1>
+           <br><br>
 
-        <form action="" method="POST">
+           <?php
+        if(isset($_SESSION['login'])){
+            echo $_SESSION['login'];
+            unset($_SESSION['login']);
+        }
+
+
+        ?>
+        <form action="" method="POST" class="text-center">
             <label>USERNAME</label>
             <input type="text" name="username" placeholder="username">
+            <br><br>
+            <label>PASSWORD</label>
+            <input type="password" name="password" placeholder="password">
+            <br><br>
+            <input type="submit" name="submit" value="login" class="btn-primary">
+            <br><br>
         </form>
         <p class="text-center">Created by-<a href="">CHANUKA RANDITHA</a></p>
     </div>
+
+    <?php 
+     if(isset($_POST['submit'])){
+      echo  $username = $_POST['username'];
+      echo $password = md5($_POST['password']);
+
+      $sql = "SELECT * FROM tbl_admin WHERE username='$username' AND password='$password'";
+
+      $res = mysqli_query($conn,$sql);
+
+      $count=mysqli_num_rows($res);
+
+      if($count==1){
+        $_SESSION['login'] = "<div class='success'>Login Successfully.</div>";
+
+        header('location:'.SITEURL.'admin/');
+      }else{
+        $_SESSION['login'] = "<div class='error'>username and password didi not match.</div>";
+
+        header('location:'.SITEURL.'admin/login.php');
+      }
+     }
+    
+    ?>
 </body>
 </html>
