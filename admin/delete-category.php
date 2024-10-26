1e78a91ee32508b177d5bd356aad189f382b5599
Chanuka999 <chanukaranditha99@gmail.com>
2024-10-26 15:56:51 -0700

    delete update file

--- admin/delete-category.php
+++ admin/delete-category.php
@@ -2,7 +2,35 @@
 include('../config/constants.php');
 
 if(isset($_GET['id']) AND isset($_GET['image_name'])){
-  echo "get values and details";
+  
+    $id=$_GET['id'];
+    $image_name=$_GET['image_name'];
+
+    if($image_name !=""){
+     $path = "../images/category/".$image_name;
+
+     $remove = unlink($path);
+
+     if($remove==false){
+          $_SESSION['remove'] = "<div class='error'>Fail to remove category image.</div>";
+
+          header('location'.SITEURL.'admin/manege-category.php');
+          die();
+     }
+    }
+    $sql = "DELETE FROM tbl_category WHERE id=$id";
+
+    $res = mysqli_query($conn,$sql);
+
+    if($res==true){
+        $_SESSION['delete'] = "<div class='success'>Category Deleted Successfully.</div>";
+
+        header('location:'.SITEURL.'admin/manege-category.php');
+    }else{
+        $_SESSION['delete'] = "<div class='success'>Failed to delete Category.</div>";
+
+        header('location:'.SITEURL.'admin/manege-category.php');
+    }
 }else{
   header('location:'.SITEURL.'admin/manege-category.php');
 }
