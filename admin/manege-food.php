9ca860f2bbc3f0fc0ace8ea64788939a3200669e
Chanuka999 <chanukaranditha99@gmail.com>
2024-10-31 16:42:30 +0530

    update

--- admin/manege-food.php
+++ admin/manege-food.php
@@ -21,50 +21,63 @@
 <table class="tbl-full">
   <tr>
     <th>S.N</th>
-    <th>Full Name</th>
-    <th>Username</th>
+    <th>title</th>
+    <th>price</th>
+    <th>image</th>
+    <th>Featured</td>
+    <th>Active</th>
     <th>Action</th>
   </tr>
 
-  <tr>
-    <td>1</td>
-    <td>chanuka</td>
-    <td>chanuu</td>
+  <?php
+   $sql = "SELECT * FROM tbl_food";
+
+   $res=mysqli_query($conn,$sql);
+
+    $count = mysqli_num_rows($res);
+
+$sn=1;
+
+    if($count>0){
+      while($row=mysqli_fetch_assoc($res)){
+        $id=$row['id'];
+        $title=$row['title'];
+        $price=$row['price'];
+        $image_name=$row['image_name'];
+        $featured=$row['featured'];
+        $active = $row['active'];
+        ?>
+        <tr>
+    <td><?php echo $sn++; ?></td>
+    <td><?php echo $title; ?></td>
+    <td><?php echo $price; ?></td>
+    <td><?php 
+      if($image_name == ""){
+        echo "<div class='error'>Image not added.</div>";
+      }else{
+        ?>
+   <img src="<?php echo SITEURL ?>images/food/<?php echo $image_name; ?>" width="100px">
+        <?php
+      }
+    ?></td>
+    <td><?php echo $featured; ?></td>
+    <td><?php echo $active; ?></td>
     <td>
-      <a href="#" class="btn-secondery">Update account</a>
-      <a href="#" class="btn-danger">delete account</a>
+      <a href="#" class="btn-secondery">Update Food</a>
+      <a href="#" class="btn-danger">delete Food</a>
     </td>
   </tr>
 
-  <tr>
-    <td>1</td>
-    <td>chanuka</td>
-    <td>chanuu</td>
-    <td>
-    <a href="#" class="btn-secondery">Update account</a>
-    <a href="#" class="btn-danger">delete account</a>
-    </td>
-  </tr>
+        <?php
+      }
+    }else{
+      echo "<tr><td colspan='7' class='errors'>Food not added yet.</td></tr>";
+    }
 
-  <tr>
-    <td>1</td>
-    <td>chanuka</td>
-    <td>chanuu</td>
-    <td>
-    <a href="#" class="btn-secondery">Update account</a>
-    <a href="#" class="btn-danger">delete account</a>
-    </td>
-  </tr>
 
-  <tr>
-    <td>1</td>
-    <td>chanuka</td>
-    <td>chanuu</td>
-    <td>
-    <a href="#" class="btn-secondery">Update account</a>
-    <a href="#" class="btn-danger">delete account</a>
-    </td>
-  </tr>
+  ?>
+  
+ 
 </table>
 
    </div>
