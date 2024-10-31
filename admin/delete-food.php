2dcaaa76ed6b252d20b7256fd07280c8b3cbb847
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-01 00:01:44 +0530

    new commit

--- admin/delete-food.php
+++ admin/delete-food.php
@@ -3,9 +3,35 @@ include('../config/constants.php');
 
 
 if(isset($_GET['id']) && isset($_GET['image_name'])){
-   echo "derected";
+  
+   $id=$_GET['id'];
+   $image_name=$_GET['image_name'];
+
+   if($image_name !=""){
+      $path = "../images/food/".$image_name;
+
+      $remove = unlink($path);
+
+      if($remove==false){
+         $_SESSION['upload'] = "<div class='error'>Failed to remove image file.</div>";
+         header('location:'.SITEURL.'admin/manege-food.php');
+         die();
+      }
+   }
+
+   $sql = "DELETE FROM tbl_food WHERE id=$id";
+
+   $res = mysqli_query($conn,$sql);
+
+   if($res==true){
+           $_SESSION['delete'] = "<div class='success'>Food deleted Successfully.</div>";
+           header('location:'.SITEURL.'admin/manege-food.php');
+   }else{
+           $_SESSION['delete'] = "<div class='error'>Failed to delete food.</div>";
+           header('location:'.SITEURL.'admin/manege-food.php');
+   }
 }else{
-   $_SESSION['delete'] = "<div class='error'>unaothorized Method.</div>";
+   $_SESSION['unauthorize'] = "<div class='error'>unauthorized Method.</div>";
    header('location:'.SITEURL.'admin/manege-food.php');
 }
 
