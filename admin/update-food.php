9e5719b92ac6713479aee9b7a29f5b1f342247f4
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-01 17:55:22 +0530

    update food

--- admin/update-food.php
+++ admin/update-food.php
@@ -41,19 +41,30 @@ if(isset($_GET['id'])){
             <tr>
                 <td>Description</td>
                 <td>
-                    <textarea name="description" cols="30" rows="5"><?php $description; ?></textarea>
+                    <textarea name="description" cols="30" rows="5"><?php echo  $description; ?></textarea>
                 </td>
             </tr>
            <tr>
             <td>Price</td>
             <td>
-                <input type="number" name="price" placeholder="enter price">
+                <input type="number" name="price" value="<?php echo $price; ?>">
             </td>
            </tr>
            <tr>
             <td>Current Image</td>
             <td>
-               <p>display the image if available</p>
+               <?php
+
+               if($current_image== ""){
+                echo "<div class='error'>Image not availble.</div>";
+                
+               }else{
+                ?>
+                    <img src="<?php echo SITEURL;  ?>images/food/<?php echo $current_image;?>" width="100px">
+                <?php
+               }
+
+              ?>
             </td>
            </tr>
 
@@ -80,7 +91,7 @@ if(isset($_GET['id'])){
 
                   //echo "<option value='$category_id'>$category_title</option>";
                   ?>
-                     <option value="<?php echo $category_id; ?>"><?php echo $category_title; ?></option>    
+                     <option <?php if($current_category==$category_id){echo "selected";} ?>value="<?php echo $category_id; ?>"><?php echo $category_title; ?></option>    
                     <?php
                 }
             }else{
@@ -100,23 +111,98 @@ if(isset($_GET['id'])){
             <tr>
                     <td>Featured:</td>
                     <td>
-                        <input type="radio" name="featured" value="yes">Yes
-                        <input type="radio" name="featured" value="No">No
+                        <input <?php if($featured=="yes") {echo "ckecked";} ?> type="radio" name="featured" value="yes">Yes
+                        <input  <?php if($featured=="No") {echo "ckecked";} ?> type="radio" name="featured" value="No">No
                     </td>
                 </tr>
                 <tr>
                     <td>Active:</td>
                     <td>
-                        <input type="radio" name="active"value="yes">Yes
-                        <input type="radio" name="active"value="No">No 
+                        <input  <?php if($active=="yes") {echo "ckecked";} ?> type="radio" name="active" value="yes">Yes
+                        <input  <?php if($active=="No") {echo "ckecked";} ?> type="radio" name="active" value="No">No 
                     </td>
                 </tr>
                 <tr>
                     <td colspan="2">
+                    <input type="hidden" name="id" value="<?php echo $id; ?>">
+                    <input type="hidden" name="current_image" value="<?php echo $current_image; ?>">
                         <input type="submit" name="submit" value="add category" class="btn-secondery">
                     </td>
                 </tr>
 </table>
+<form>
+
+<?php
+ if(isset($_POST['submit'])){
+    $id=$_POST['id'];
+    $title=$_POST['title'];
+    $description=$_POST['description'];
+    $price=$_POST['price'];
+    $current_image=$_POST['current_image'];
+    $category = $_POST['category'];
+    $featured=$_POST['featured'];
+    $active=$_POST['active'];
+
+
+    if(isset($_FILES['image']['name'])){
+        $image_name = $_FILES['images']['name'];
+
+        if($image_name != ""){
+            $ext = end(explode('.',$image_name));
+
+            $image_name = "Food_Name_".rand(0000,9999).'.'.$ext;
+      
+      
+                  $src_path=$_FILES['image']['tmp_name'];
+                  $dst_path = "../images/food/".$image_name;
+      
+                  $upload = move_uploaded_file($src_path,$dst_path);
+
+                  if($upload==false){
+                    $_SESSION['upload']="<div class='error'>Failed to upload image</div>";
+    
+                    header('location:'.SITEURL.'admin/manege-food.php');
+                    die();
+        }
+        if($current_image!==""){
+            $remove_path = "../images/food/".$current_image;
+
+            $remove =unlink($remove_path);
+
+            if($remove==false){
+               $_SESSION['remove-failed'] = "<div class='error'>Failed to remove current image</div>";
+               header('location:'.SITEURL.'admin/manege-food.php');
+               die();
+            }
+        }
+    }
+    }else{
+        $image_name=$current_image;
+    }
+
+    $sql3 = "UPDATE tbl_food SET
+    title='$title',
+    description='$description',
+    price='$price',
+    image_name='$image_name',
+    category_id='$category',
+    featured='$featured',
+    active='$active'
+    WHERE id=$id";
+
+    $res3 = mysqli_query($conn,$sql3);
+
+    if($res3==true){
+           $_SESSION['update'] = "<div class='success'>Food updated Successfully.</div>";
+           header('location:'.SITEURL.'admin/manege-food.php');
+    }else{
+         $_SESSION['update'] = "<div class='error'>Faile to update Food.</div>";
+           header('location:'.SITEURL.'admin/manege-food.php');
+    }
+
+ }
+
+?>
 </div>
 </div>
 <?php  include('partials/footer.php'); ?>
