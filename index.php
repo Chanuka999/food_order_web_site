2e254fec549e2b185beeb046197ca90fb3719e7f
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-01 18:25:26 +0530

    apply php

---
+++ index.php
@@ -0,0 +1,165 @@
+<?php  include('partials-front/menu.php');  ?>
+
+    <section class="food-search text-center">
+        <div class="container">
+            <form action="form-search.html" method="POST">
+                <input type="search" name="search" placeholder="Search for foods" required>
+                <input type="submit" name="submit" value="search" class="btn btn-primary"> 
+            </form>
+        </div>
+
+    </section>
+
+    <section class="categories">
+        <div class="container">
+            <h2 class="text-center">Explore Foods</h2>
+
+            <a href="category-foods.html">
+            <div class="box-3 float-container">
+                <img src="images/burger.jpg" alt="burger" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">Burger</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/pizza.jpg" alt="pizza" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">pizza</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/momo.jpg" alt="Memo" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">Memo</h3>
+            </div>
+        </a>
+        <div class="clearfix"></div>
+        </div>
+
+    </section>
+
+    <section class="food-menu">
+        <div class="container">
+            <h2 class="text-center">Food Menu</h2>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-pizza.jpg" alt="chicke Hawin pizza" class="img-responsive img-curve">
+
+                </div>
+                <div class="food-menu-desc">
+                    <h4>Food Title</h4>
+                    <p class="food-price">$2.3</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce,Chicken,and organic vegetables.
+                    </p>
+                    <br>
+
+                    <a href="order.html">Order Now</a>
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-burger.jpg" alt="chicke Hawin pizza" class="img-responsive img-curve">
+
+                </div>
+                <div class="food-menu-desc">
+                    <h4>Smoky Burger</h4>
+                    <p class="food-price">$2.3</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce,Chicken,and organic vegetables.
+                    </p>
+                    <br>
+
+                    <a href="#">Order Now</a>
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-burger.jpg" alt="chicke Hawin pizza" class="img-responsive img-curve">
+
+                </div>
+                <div class="food-menu-desc">
+                    <h4>New Burger</h4>
+                    <p class="food-price">$2.3</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce,Chicken,and organic vegetables.
+                    </p>
+                    <br>
+
+                    <a href="#">Order Now</a>
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-pizza.jpg" alt="chicke Hawin pizza" class="img-responsive img-curve">
+
+                </div>
+                <div class="food-menu-desc">
+                    <h4>Nice Burger</h4>
+                    <p class="food-price">$2.3</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce,Chicken,and organic vegetables.
+                    </p>
+                    <br>
+
+                    <a href="#">Order Now</a>
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-pizza.jpg" alt="chicke Hawin pizza" class="img-responsive img-curve">
+
+                </div>
+                <div class="food-menu-desc">
+                    <h4>Food Title</h4>
+                    <p class="food-price">$2.3</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce,Chicken,and organic vegetables.
+                    </p>
+                    <br>
+
+                    <a href="#">Order Now</a>
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-momo.jpg" alt="chicke Hawin pizza" class="img-responsive img-curve">
+
+                </div>
+                <div class="food-menu-desc">
+                    <h4>Food Title</h4>
+                    <p class="food-price">$2.3</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce,Chicken,and organic vegetables.
+                    </p>
+                    <br>
+
+                    <a href="#">Order Now</a>
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-momo.jpg" alt="chicke Hawin pizza" class="img-responsive img-curve">
+
+                </div>
+                <div class="food-menu-desc">
+                    <h4>Chicken Steam Momo</h4>
+                    <p class="food-price">$2.3</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce,Chicken,and organic vegetables.
+                    </p>
+                    <br>
+
+                    <a href="#">Order Now</a>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <p class="text-center">
+          <a href="#">See All Foods</a>
+        </p>
+    </section>
+    <?php  include('partials-front/footer.php');  ?>
