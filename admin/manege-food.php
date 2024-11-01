9294146b80831de1d409480afae92b8ace780813
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-01 17:55:12 +0530

    new commit

--- admin/manege-food.php
+++ admin/manege-food.php
@@ -28,6 +28,11 @@
         echo $_SESSION['unauthorize'];
         unset($_SESSION['unauthorize']);
       }
+
+      if(isset($_SESSION['update'])){
+        echo $_SESSION['update'];
+        unset($_SESSION['update']);
+      }
       ?>
 
 
