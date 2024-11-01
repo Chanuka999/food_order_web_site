5bb1410828868bf1aa4e5da4d48de322d1e0adec
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-01 18:24:03 +0530

    apply php

---
+++ categories.php
@@ -0,0 +1,98 @@
+<?php  include('partials-front/menu.php');  ?>
+
+      <section class="categories">
+        <div class="container">
+            <h2 class="text-center">Explore Foods</h2>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/burger.jpg" alt="burger" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">Burger</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/pizza.jpg" alt="pizza" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">pizza</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/momo.jpg" alt="Memo" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">Memo</h3>
+            </div>
+        </a>
+
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/burger.jpg" alt="burger" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">Burger</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/pizza.jpg" alt="pizza" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">pizza</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/momo.jpg" alt="Memo" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">Memo</h3>
+            </div>
+        </a>
+
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/burger.jpg" alt="burger" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">Burger</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/pizza.jpg" alt="pizza" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">pizza</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/momo.jpg" alt="Memo" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">Memo</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/burger.jpg" alt="burger" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">Burger</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/pizza.jpg" alt="pizza" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">pizza</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/momo.jpg" alt="Memo" class="img-responsive img-curve">
+
+                <h3 class="float-text text-container">Memo</h3>
+            </div>
+        </a>
+        <div class="clearfix"></div>
+        </div>
+
+    </section>
+    <?php  include('partials-front/footer.php');  ?>
+    
