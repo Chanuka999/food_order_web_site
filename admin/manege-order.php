f2b4c37c3b510f72038bdaad27cc4b3b3210f529
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-09 00:04:24 +0530

    use to php

--- admin/manege-order.php
+++ admin/manege-order.php
@@ -8,50 +8,38 @@
 <table class="tbl-full">
   <tr>
     <th>S.N</th>
-    <th>Full Name</th>
-    <th>Username</th>
-    <th>Action</th>
+    <th>Food</th>
+    <th>price</th>
+    <th>qty</th>
+    <th>total</th>
+    <th>order date</th>
+    <th>status</th>
+    <th>customer name</th>
+    <th>Contact</th>
+    <th>Email</th>
+    <th>Address</th>
+    <th>Actions</th>
+   
   </tr>
 
   <tr>
     <td>1</td>
     <td>chanuka</td>
     <td>chanuu</td>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
     <td>
       <a href="#" class="btn-secondery">Update account</a>
-      <a href="#" class="btn-danger">delete account</a>
     </td>
   </tr>
 
-  <tr>
-    <td>1</td>
-    <td>chanuka</td>
-    <td>chanuu</td>
-    <td>
-    <a href="#" class="btn-secondery">Update account</a>
-    <a href="#" class="btn-danger">delete account</a>
-    </td>
-  </tr>
-
-  <tr>
-    <td>1</td>
-    <td>chanuka</td>
-    <td>chanuu</td>
-    <td>
-    <a href="#" class="btn-secondery">Update account</a>
-    <a href="#" class="btn-danger">delete account</a>
-    </td>
-  </tr>
-
-  <tr>
-    <td>1</td>
-    <td>chanuka</td>
-    <td>chanuu</td>
-    <td>
-    <a href="#" class="btn-secondery">Update account</a>
-    <a href="#" class="btn-danger">delete account</a>
-    </td>
-  </tr>
+ 
 </table>
 
    </div>
