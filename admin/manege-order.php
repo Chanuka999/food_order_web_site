76ca8e5a91e066f2ce0927287f04dc2d89bfbe88
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-09 20:04:23 +0530

    connecting oeder page

--- admin/manege-order.php
+++ admin/manege-order.php
@@ -5,6 +5,14 @@
    <h1>MANEGE ORDER</h1>
   
 <br><br><br>
+ <?php
+   if(isset($_SESSION['update'])){
+    echo $_SESSION['update'];
+    unset($_SESSION['update']);
+   }
+ ?>
+ <br><br>
+
 <table class="tbl-full">
   <tr>
     <th>S.N</th>
@@ -22,22 +30,73 @@
    
   </tr>
 
-  <tr>
-    <td>1</td>
-    <td>chanuka</td>
-    <td>chanuu</td>
-    <th></th>
-    <th></th>
-    <th></th>
-    <th></th>
-    <th></th>
-    <th></th>
-    <th></th>
-    <th></th>
-    <td>
-      <a href="#" class="btn-secondery">Update account</a>
-    </td>
-  </tr>
+   <?php
+    $sql ="SELECT * FROM tbl_order ORDER BY id DESC";
+
+    $res = mysqli_query($conn,$sql);
+
+    $count = mysqli_num_rows($res);
+
+   $sn = 1;
+
+    if($count>0){
+      while($row=mysqli_fetch_assoc($res)){
+        $id=$row['id'];
+        $food =$row['food'];
+        $price = $row['price'];
+        $qty = $row['qty'];
+        $total = $row['total'];
+        $order_date = $row['order_date'];
+        $status = $row['status'];
+        $customer_name =$row['customer_name'];
+        $customer_contact =$row['customer_contact'];
+        $customer_email = $row['customer_email'];
+        $customer_address = $row['customer_address'];
+
+          ?>
+         <tr>
+            <td><?php echo $sn++; ?></td>
+            <td><?php echo $food; ?></td>
+            <td>c<?php echo $price; ?></td>
+            <th><?php echo $qty; ?></th>
+            <th><?php echo $total; ?></th>
+            <th><?php echo $order_date; ?></th>
+
+            <th>
+              <?php 
+               if($status == "ordered"){
+                echo "<label>$status</label>";
+               }elseif($status == "on delevery"){
+                echo "<label style='color:orange;'>$status</label>";
+               }elseif($status == "Deleverd"){
+                echo "<label style='color:green;'>$status</label>";
+               }elseif($status == "cancelled"){
+                echo "<label style='color:red;'>$status</label>";
+               }
+              ?>
+            </th>
+
+            <th><?php echo $customer_name; ?></th>
+            <th><?php echo $customer_contact; ?></th>
+            <th><?php echo $customer_email; ?></th>
+            <th><?php echo $customer_address; ?></th>
+            <td>
+              <a href="<?php echo SITEURL; ?>admin/update-order.php?id=<?php echo $id; ?>" class="btn-secondery">Update account</a>
+            </td>
+      </tr>
+
+           <?php
+
+      }
+    }else{
+      echo "<tr><td colspan='12' class='error'>Order not found.</td></tr>";
+    }
+
+   ?>
+
+
+
+  
 
  
 </table>
